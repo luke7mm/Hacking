cc4598fc8a839da37048b55728c7769db49d2e88
Luke Mahan <Mahan.Luke@gmail.com>
2021-11-08 23:13:10 -0500

    Add files via upload

---
+++ Simple divisor program.js
@@ -0,0 +1,13 @@
+var numbers = 10;
+
+function divideBy(number, divisor) {
+  return number / divisor;
+}
+
+console.log(divideBy(numbers, 2));
+
+if (divideBy < 7) {
+  console.log("Your input is too low");
+}
+
+
