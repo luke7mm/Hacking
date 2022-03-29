d21b4df9d53b8dd329d8b038247c1edfaa4a6f5b
LM <Mahan.Luke@gmail.com>
2022-03-29 19:37:35 -0400

    Add files via upload

---
+++ scrambled_names/name.py
@@ -0,0 +1,18 @@
+# Scrambled names
+# Simple program to scramble up your name
+#
+
+import random
+from random import shuffle
+
+print('Welcome to the name scrambler!\n\nThis is a simple program which scrambles up your name')
+
+name = input('Please enter your first name here: ')
+
+# Converting str to char and shuffle
+
+char_name = list(name)
+random.shuffle(char_name)
+result = ''.join(char_name)
+
+print('Your shuffled name is:', result)
