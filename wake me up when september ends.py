334a6a10f5d6edad8ff4139cb831396196c4e203
LM <Mahan.Luke@gmail.com>
2022-03-29 21:44:32 -0400

    Add files via upload

---
+++ wake me up when september ends.py
@@ -0,0 +1,13 @@
+# Practicing Python time module
+
+import time
+
+print("Wake me up when September ends")
+
+time.sleep(10)
+
+print("September is over")
+
+
+epoch_sec = time.time()
+print("Seconds since epoch =", epoch_sec)
