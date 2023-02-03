96af9a0a95df3f2d9a3c135832d6792a6490be3d
Luke <u32luke@gmail.com>
2023-02-03 13:30:20 -0500

    Simple breath counter program

---
+++ breathCount.py
@@ -0,0 +1,22 @@
+import time
+
+count = 0
+
+while (count < 100):
+    print("Breath in")
+    time.sleep(1)
+    print(1)
+    time.sleep(1)
+    print(2)
+    time.sleep(1)
+    print(3)
+    time.sleep(1)
+    print("Breath out")
+    time.sleep(1)
+    print(1)
+    time.sleep(1)
+    print(2)
+    time.sleep(1)
+    print(3)
+    time.sleep(1)
+    count += 1
